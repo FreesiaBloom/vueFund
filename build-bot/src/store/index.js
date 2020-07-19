f30c1ac4be4607d424a2604dfec19ed146c73599
FreesiaBloom <agata.jasinska215@gmail.com>
2020-07-19 13:26:58 +0200

    namespacing modules

--- build-bot/src/store/index.js
+++ build-bot/src/store/index.js
@@ -1,12 +1,14 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 
-import robotModule from './modules/robots';
+import robotsModule from './modules/robots';
+import usersModule from './modules/users';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    robots: robotModule,
+    robots: robotsModule,
+    users: usersModule,
   },
 });
